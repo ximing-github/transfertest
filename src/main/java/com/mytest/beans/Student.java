a63a0f6dfe62cea9ef0bdf0e003c974787e331b4
xm <1546968780@qq.com>
2021-05-08 11:02:47 +0800

    2

---
+++ src/main/java/com/mytest/beans/Student.java
@@ -0,0 +1,14 @@
+package com.mytest.beans;
+
+/**
+ * ClassName:Student
+ * Package: com.mytest.beans
+ * Description:
+ *
+ * @Date: 2021/5/8 11:01
+ * @Author: 惜名
+ */
+public class Student {
+    private String name;
+    private String id;
+}
